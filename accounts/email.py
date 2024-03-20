7ab6e7160f4ab43a981efe55d059d772cc827cd9
Muntasir-Arin <muntasirarin@gmail.com>
2024-03-21 03:47:39 +0600

    commit

--- accounts/email.py
+++ accounts/email.py
@@ -2,16 +2,16 @@ from djoser import email
 
 
 class ActivationEmail(email.ActivationEmail):
-    template_name = 'email/activation.html'
+    template_name = 'account/activation.html'
 
 
 class ConfirmationEmail(email.ConfirmationEmail):
-    template_name = 'email/confirmation.html'
+    template_name = 'account/confirmation.html'
 
 
 class PasswordResetEmail(email.PasswordResetEmail):
-    template_name = 'email/password_reset.html'
+    template_name = 'account/password_reset.html'
 
 
 class PasswordChangedConfirmationEmail(email.PasswordChangedConfirmationEmail):
-    template_name = 'email/password_changed_confirmation.html'
+    template_name = 'account/password_changed_confirmation.html'
